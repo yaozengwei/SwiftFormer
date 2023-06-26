64680b8444fa691de5439831c0b695d3c47e1e16
yaozengwei <yaozengwei@outlook.com>
2023-06-26 11:53:03 +0800

    add swiftformer_local.py, which use local query

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,2 @@
 from .swiftformer import SwiftFormer_XS, SwiftFormer_S, SwiftFormer_L1, SwiftFormer_L3
+from .swiftformer_local import SwiftFormer_XS_local, SwiftFormer_S_local, SwiftFormer_L1_local, SwiftFormer_L3_local
